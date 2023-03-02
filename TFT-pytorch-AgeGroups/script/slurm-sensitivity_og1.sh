3b7da97ce2caa18dab57c093fc53f55ab98433ac
Tyler Valentine <Tyv132@protonmail.com>
2023-03-02 00:05:57 -0500

    added current age subgroup experiments

---
+++ TFT-pytorch-AgeGroups/script/slurm-sensitivity_og1.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --job-name="AGE4049"
+#SBATCH --output=outputs/AGE4049_sensitivity.out
+#SBATCH --error=outputs/AGE4049_sensitivity_error.err
+#SBATCH --partition=gpu
+#SBATCH --time=20:00:00
+#SBATCH --account=ds--6013
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=32GB
+
+# this is for when you are using singularity
+singularity run --nv ./tft_pytorch.sif python sensitivity_analysis_og1.py
